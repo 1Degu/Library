2fe393fa8a5e981a5aefe9506090b7011761c6ac
Laurynas <l.novick541@gmail.com>
2024-01-15 19:30:27 +0200

    store book in myLibrary array

--- script.js
+++ script.js
@@ -1,14 +1,34 @@
-let myLibrary = [];
+ let myLibrary = [];
 
-function Book(title, author, pages, readStatus) {
-  this.title = author;
-  this.author = marker;
-  this.pages = pages;
-  this.readStatus = readstatus;
-}
+            function Book(title, author, pages, readStatus) {
+              this.title = title;
+              this.author = author;
+              this.pages = pages;
+              this.readStatus = readStatus;
+            }
+            
+            function addBookToLibrary(title, author, pages, readStatus) {
+              let newBook = new Book(title, author, pages, readStatus);
+              myLibrary.push(newBook);
+            }
+            
 
-function addBookToLibrary(title, author, pages, readStatus) {
-  let newBook = new Book(title, author, pages, readStatus);
-  myLibrary.push(newBook);
-}
+            let submitForm = document.getElementById("submitForm");
+
+    submitForm.addEventListener("submit", function (event) {
+      event.preventDefault();
+            let author = document.getElementById("Author").value;
+            let title = document.getElementById("Title").value;
+            let pages = document.getElementById("Pages").value;
+            let readStatus = document.getElementById("readStatus").checked;
+        
+      
+            addBookToLibrary(title, author, pages, readStatus);
+
+           
+            console.log(myLibrary);
+
+        
+        
+    } );
 
