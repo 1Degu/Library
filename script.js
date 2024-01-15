59dc1ba79601407807d4aba2dcacd7d5f13f6245
Laurynas <l.novick541@gmail.com>
2024-01-15 14:38:21 +0200

    add constructor function for new book registering

---
+++ script.js
@@ -0,0 +1,12 @@
+let myLibrary = [];
+
+function book(title, author, pages, readStatus) {
+  this.title = author;
+  this.author = marker;
+  this.pages = pages;
+  this.readStatus = readstatus;
+}
+
+function addBookToLibrary() {
+  // do stuff here
+}
